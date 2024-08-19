f29f8d8c42e388805c9fa13d692feca9f62de7b3
Ian Rodrigues <ianmrdrigues@gmail.com>
2024-08-19 11:58:39 -0300

    correção métodos de acesso

--- Questao_2/main.rb
+++ Questao_2/main.rb
@@ -8,4 +8,3 @@ c1.depositar(100)
 c1.consultar_saldo
 c1.sacar(99)
 c1.consultar_saldo
-#c1.saldo = 1000000 #Caso tente executar essa linha irá retornar um erro dizendo que 'saldo=' é um método privado.
